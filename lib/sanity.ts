6e13f6d387ac4dd3b0d5c3e88ab5faf7747c2475
santullidev <131721798+santullidev@users.noreply.github.com>
2026-04-02 14:06:16 -0300

    fix: Robust Sanity client and apiVersion for Vercel build

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -3,9 +3,9 @@ import imageUrlBuilder from '@sanity/image-url'
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'obhj76tx',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01',
   useCdn: true,
 })
 
