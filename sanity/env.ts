84f3c133383dbebee6befdb9705954e3f553b30f
santullidev <131721798+santullidev@users.noreply.github.com>
2026-04-02 14:13:10 -0300

    fix: reconcile project name to skillglass and standardize sanity apiVersion

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-03-29'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
